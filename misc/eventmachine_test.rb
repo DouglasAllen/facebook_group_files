dfab708b1216fd10b1f7c2fde0051be9455ad70a
Douglas <kb9agt@gmail.com>
2018-01-13 08:36:49 -0600

    double quotes and rubocop?

--- misc/eventmachine_test.rb
+++ misc/eventmachine_test.rb
@@ -9,24 +9,24 @@ class KeyboardHandler < EM::Connection
   include EM::Protocols::LineText2
 
   def post_init
-    print "> "
+    print '> '
   end
 
   def receive_line(line)
     line.chomp!
     line.gsub!(/^\s+/, '')
 
-    case(line)
+    case line
     when /^get (.*)$/ then
-      site = $1.chomp
+      site = Regexp.last_match(1).chomp
       sites = site.split(',')
 
       multi = EM::MultiRequest.new
       sites.each do |s|
         multi.add(EM::HttpRequest.new(s).get)
       end
-      multi.callback {
-        puts ""
+      multi.callback do
+        puts ''
         multi.responses[:succeeded].each do |h|
           pp h.response_header.status
           pp h.response_header
@@ -34,23 +34,23 @@ class KeyboardHandler < EM::Connection
         multi.responses[:failed].each do |h|
           puts "#{h.inspect} failed"
         end
-        print "> "
-      }
-      print "> "
+        print '> '
+      end
+      print '> '
 
     when /^exit$/ then
       EM.stop
 
     when /^help$/ then
-      puts "get URL[,URL]*   - gets a URL"
-      puts "exit      - exits the app"
-      puts "help      - this help"
-      print "> "
+      puts 'get URL[,URL]*   - gets a URL'
+      puts 'exit      - exits the app'
+      puts 'help      - this help'
+      print '> '
     end
   end
 end
 
-EM::run {
+EM.run do
   EM.open_keyboard(KeyboardHandler)
-}
-puts "Finished"
+end
+puts 'Finished'
--- misc/sun.rb
+++ misc/sun.rb
@@ -3,61 +3,119 @@ include Math
 D2R = PI / 180.0
 R2D = 180.0 / PI
 
+# Geometric Mean Longitude
 def gml_sun(jc)
   (280.46646 + jc * (36_000.76983 + jc * 0.0003032)) % 360.0
 end
 
+# Geometric Mean Anomaly
 def gma_sun(jc)
   357.52911 + jc * (35_999.05029 - 0.0001537 * jc) % 360.0
 end
 
+# Earth Eliptical Orbit
 def eeo(jc)
   0.016708634 - jc * (0.000042037 + 0.0000001267 * jc)
 end
 
+# Equation of Center
 def sun_eoc(jc)
-  sin((gma_sun(jc) * D2R)) * (1.914602 - jc * (0.004817 + 0.000014 * jc)) +
-    sin(D2R * (2 * gma_sun(jc))) * (0.019993 - 0.000101 * jc) +
-    sin(D2R * (3 * gma_sun(jc))) * 0.000289
+  eoc_1(jc) +
+  eoc_2(jc) +
+  eoc_3(jc)
 end
 
+def eoc_1(jc)
+  sin((gma_sun(jc) * D2R)) * (1.914602 - jc * (0.004817 + 0.000014 * jc))
+end
+
+def eoc_2(jc)
+  sin(D2R * (2 * gma_sun(jc))) * (0.019993 - 0.000101 * jc)
+end
+
+def eoc_3(jc)
+  sin(D2R * (3 * gma_sun(jc))) * 0.000289
+end
+
+# True Longitude
 def sun_tl(jc)
   sun_eoc(jc) + gml_sun(jc)
 end
 
+# Apparent Longitude
 def sun_al(jc)
   sun_tl(jc) - 0.00569 - 0.00478 * sin(D2R * (125.04 - 1934.136 * jc))
 end
 
+# Mean Obliquity Earth
 def mobe(jc)
   23 + (26 + ((21.448 - jc * (46.815 + jc * (0.00059 -
   jc * 0.001813)))) / 60.0) / 60.0
 end
 
+# Obliquity Correction
 def obc(jc)
   mobe(jc) + 0.00256 * cos(D2R * (125.04 - 1934.136 * jc))
 end
 
+# Declination
 def sun_dec(jc)
   R2D * asin(sin(D2R * obc(jc)) * sin(D2R * sun_al(jc)))
 end
 
+# Tangent 1/2 Obliquity Squared
 def foo_y(jc)
   tan(D2R * (obc(jc) / 2.0)) * tan(D2R * (obc(jc) / 2.0))
 end
 
+# Equation of Time
 def eot(jc)
-  4.0 * R2D * (foo_y(jc) * sin(2.0 * D2R * gml_sun(jc)) -
-  2.0 * eeo(jc) * sin(D2R * gma_sun(jc)) +
-  4.0 * eeo(jc) * foo_y(jc) * sin(D2R * gma_sun(jc)) * cos(2.0 * D2R * gml_sun(jc)) -
-  0.5 * foo_y(jc) * foo_y(jc) * sin(4.0 * D2R * gml_sun(jc)) -
-  1.25 * eeo(jc) * eeo(jc) * sin(2.0 * D2R * gma_sun(jc)))
+  4.0 * R2D * (
+    eot_t1(jc) -
+    eot_t2(jc) +
+    eot_t3(jc) -
+    eot_t4(jc) -
+    eot_t5(jc)
+  )
 end
 
-def sun_ha(jc, lat)
-  R2D * acos((cos(D2R * 90.833) / (cos(D2R * lat) * cos(D2R * sun_dec(jc)))) -
-  tan(D2R * lat) * tan(D2R * sun_dec(jc)))
+def eot_t1(jc)
+  foo_y(jc) * sin(2.0 * D2R * gml_sun(jc))
 end
+
+def eot_t2(jc)
+  2.0 * eeo(jc) * sin(D2R * gma_sun(jc))
+end
+
+def eot_t3(jc)
+  4.0 * eeo(jc) * foo_y(jc) * sin(D2R * gma_sun(jc)) * cos(2.0 * D2R * gml_sun(jc))
+end
+
+def eot_t4(jc)
+  0.5 * foo_y(jc) * foo_y(jc) * sin(4.0 * D2R * gml_sun(jc))
+end
+
+def eot_t5(jc)
+  1.25 * eeo(jc) * eeo(jc) * sin(2.0 * D2R * gma_sun(jc))
+end
+
+# Horizon Angle = angle degrees for half a day
+def sun_ha(jc, lat)
+  R2D * acos(trig_1(jc, lat) - trig_2(jc, lat))
+end
+
+def zenith
+  D2R * 90.833
+end
+
+def trig_1(jc,lat)
+  cos(zenith) / (cos(D2R * lat) * cos(D2R * sun_dec(jc)))
+end
+
+def trig_2(jc, lat)
+  tan(D2R * lat) * tan(D2R * sun_dec(jc))
+end
+
 
 latitude = 41.9475
 longitude = -88.743
